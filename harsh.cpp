3dc669f159645d19bab87af366855d8299bd7b2a
harsh7913 <workmailharsh@gmail.com>
2024-07-03 06:01:16 +0530

    checking

---
+++ harsh.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int mod = 1e9 + 7;
+
+int main()
+{
+    int a, b, n;
+    cin >> a >> b >> n;
+
+    for (int i = 0; i < n; i++)
+        cout << "hello idiot" << endl;
+
+    cout<<"done for the day"<<endl;
+    cout<<"going through a tough phase of life"<<endl;
+    return 0;
+}
